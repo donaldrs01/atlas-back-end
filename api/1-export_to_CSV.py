ac354f716350b6bce08a792784161d6392c330ae
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-03-15 14:43:42 -0500

    needed to filter tasks by user ID

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
 
     base_url = "https://jsonplaceholder.typicode.com"
     employee_url = f'{base_url}/users/{sys.argv[1]}'
-    todo_url = f'{base_url}/todos'
+    #  Needed to filter tasks by user ID
+    todo_url = f'{base_url}/todos?userId={sys.argv[1]}'
     file_name = f'{sys.argv[1]}.csv'
 
     employee_response = requests.get(employee_url)
